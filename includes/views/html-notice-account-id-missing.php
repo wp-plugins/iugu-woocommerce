7f4571e01177e1f2c77e39c2479da118d5c6bdfa
claudiosanches <claudiosanches@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-01 04:27:28 +0000

    Version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/iugu-woocommerce/tags/1.0.6@1150741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/views/html-notice-account-id-missing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Admin View: Notice - Account ID missing.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
+<div class="error">
+	<p><strong><?php _e( 'Iugu Disabled', 'iugu-woocommerce' ); ?></strong>: <?php _e( 'You should inform your API Token.', 'iugu-woocommerce' ); ?>
+	</p>
+</div>
---
+++ includes/views/html-notice-api-token-missing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Admin View: Notice - API Token missing.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
+<div class="error">
+	<p><strong><?php _e( 'Iugu Disabled', 'iugu-woocommerce' ); ?></strong>: <?php _e( 'You should inform your Account ID.', 'iugu-woocommerce' ); ?>
+	</p>
+</div>
